a4b879135fd01fd7468700c780376d5d14eb67e9
George <zhouyu674896488@gmail.com>
2019-07-22 20:16:04 +0800

    shell

--- merge data.sh
+++ merge data.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
+input="/Users/george/Downloads/track/2018/10/08";
+output="log/merged.csv";
 
-python Nail.py -input=/Users/george/Downloads/track/2018/10/08 -output=log/merged.csv
+python Nail.py -input=input -output=output
